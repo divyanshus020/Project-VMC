e448a3327d0e08eeff6958ef7d9f734deb8c90f0
divyanshu <divyanshu>
2025-06-05 20:54:09 +0530

    feat: integrate Tailwind CSS and Ant Design into the project
    - Added Tailwind CSS and Ant Design as dependencies in package.json.
    - Removed existing CSS styles in App.css and index.css to utilize Tailwind CSS.
    - Updated App.jsx to include an Ant Design button and removed unused elements.
    - Imported Ant Design's reset CSS in main.jsx for proper styling.
    - Configured Vite to use Tailwind CSS plugin in vite.config.js.

--- jewelleryEcommerce/src/main.jsx
+++ jewelleryEcommerce/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'antd/dist/reset.css'; // For Ant Design v5+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
